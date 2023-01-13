29d47810b6cdc6df61277ad9439c766b0db17dce
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-01-13 13:17:53 -0700

    commit tree fd74360d9dab294218e5cc14f27d06e9b8a3e61c

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if(word.split('').reverse().join('') === word) {
+    return true
+  } else {
+    return false
+  }
 }
 
-/* 
+  // Write your algorithm here
+
+/* //write function
+// receive one argument: a string
+// returns true or false
+// if else statement
+// figure how to flip the word backwards
+// 
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
 */
+// the first if statement takes the word 
+//.split separates the palidrome into individual letters in a new array
+// reverse() takes that new array and flips it
+//.join pulls the array back together into a string
+// it then compares to see if it matches the original word
+// if they match the if else will sort it and return either true or false. 
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Epecting: true")
+  console.log("=>", isPalindrome("mom"))
+
+  console.log("Expecting: false")
+  console.log("=>", isPalindrome("car"))
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
